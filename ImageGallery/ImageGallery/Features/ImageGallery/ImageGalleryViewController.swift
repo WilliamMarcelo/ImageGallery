fcc6cc3cff34cbfaa710b45d2d33812268ff7656
William Gomes <williamgomes@boldint.com>
2019-11-23 11:17:34 +0000

    Project creation

---
+++ ImageGallery/ImageGallery/Features/ImageGallery/ImageGalleryViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  ImageGallery
+//
+//  Created by William Gomes on 23/11/2019.
+//  Copyright © 2019 William Gomes. All rights reserved.
+//
+
+import UIKit
+
+class ImageGalleryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
---
+++ ImageGallery/ImageGallery/SceneDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  SceneDelegate.swift
+//  ImageGallery
+//
+//  Created by William Gomes on 23/11/2019.
+//  Copyright © 2019 William Gomes. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
